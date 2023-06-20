feb424dbd16db0a3e0ca94f23cd413d2b47de805
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-20 17:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let button = document.querySelector("#btn");
+let div = document.querySelector("#colorSelect");
+button.addEventListener("click", removeElement)
+
+function removeElement(){
+    div.remove(div.selectedIndex);
+ };
